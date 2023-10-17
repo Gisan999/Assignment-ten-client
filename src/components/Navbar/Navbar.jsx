155385d48030db1832a1e85f1e581768af017490
Gisan999 <ruheatheking3@gmail.com>
2023-10-18 02:45:51 +0600

    as first added navbar and footer in client site successfully

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,90 @@
+import { NavLink } from "react-router-dom";
+import logo from '../../assets/logo.png'
+
+
+const Navbar = () => {
+
+    const navbar = <>
+        <li>  <NavLink
+            to="/"
+            className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "active" : ""
+            }
+        >
+            Home
+        </NavLink>
+        </li>
+        <li>   <NavLink
+            to="/kk"
+            className={({ isActive, isPending }) =>
+                isPending ? "pending" : isActive ? "active" : ""
+            }
+        >
+            Home
+        </NavLink></li>
+
+    </>
+
+    return (
+        <div>
+            <div className="max-w-screen-xl mx-auto">
+                <div className="navbar bg-base-100">
+                    <div className="navbar-start">
+                        <div className="dropdown">
+                            <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                            </label>
+                            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                                {navbar}
+                            </ul>
+                        </div>
+                        {/* <a className="btn btn-ghost normal-case text-xl">{logo}</a> */}
+                        <img src={logo} alt="" />
+                    </div>
+                    <div className="navbar-center hidden lg:flex">
+                        <ul className="menu menu-horizontal px-1">
+                            {navbar}
+                        </ul>
+                    </div>
+                    <div className="navbar-end">
+                        {/* <a className="btn">Button</a> */}
+                        <div className="dropdown  dropdown-end">
+                        <label tabIndex={1} className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img src="https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745" />
+                            </div>
+                        </label>
+                        <ul tabIndex={1} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                          <li>name:</li>
+                            <li><a>Settings</a></li>
+                            <li><a>Logout</a></li>
+                        </ul>
+                    </div>
+
+
+                    </div>
+
+                    {/* <div className="dropdown  dropdown-end">
+                        <label tabIndex={1} className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                            </div>
+                        </label>
+                        <ul tabIndex={1} className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52">
+                            <li>
+                                <a className="justify-between">
+                                    Profile
+                                    <span className="badge">New</span>
+                                </a>
+                            </li>
+                            <li><a>Settings</a></li>
+                            <li><a>Logout</a></li>
+                        </ul>
+                    </div> */}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
